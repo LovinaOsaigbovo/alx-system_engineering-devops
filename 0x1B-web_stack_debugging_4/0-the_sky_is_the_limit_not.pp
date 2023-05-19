d8e2b435b59552f818f2c1f457ae0d72fe2ffcd3
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-05-19 03:23:33 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-#A puppet script to set requests limit
+# Increases the amount of traffic an Nginx server can handle.
 
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ULIMIT='-n 4000'\n",
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
